f7366208345396ef4016d6ab0382bd75b2cae867
PetrovaOV <petroffov@gmail.com>
2023-03-05 16:18:19 +0400

    4.1 commit

---
+++ src/card_game.js
@@ -0,0 +1,45 @@
+const difficulty = [
+    {
+        tag: "div",
+        cls: 'box',
+        content: [{
+
+            tag: 'h1',
+            cls: 'title',
+            content: 'Выбери сложность'
+        },
+        {
+            tag: 'div',
+            cls: 'windows',
+            content: [{
+
+                tag: 'div',
+                cls: 'window',
+                content: "1"
+            },
+            {
+
+                tag: 'div',
+                cls: 'window',
+                content: "2"
+            },
+            {
+
+                tag: 'div',
+                cls: 'window',
+                content: "3"
+            }]
+        },
+        {
+            tag: 'button',
+            attrs: {
+                id: 'btn'
+            },
+            content: 'Старт',
+            cls: 'button__start'
+        }
+        ],
+    }
+];
+
+
