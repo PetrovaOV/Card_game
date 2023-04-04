9402dadb864b90fa30837088c3096c770ae48dfd
PetrovaOV <petroffov@gmail.com>
2023-04-04 22:22:50 +0400

    04042023

--- src/game_class.js
+++ src/game_class.js
@@ -1,43 +1,81 @@
 class Game {
     constructor(screen) {
         if (!(screen instanceof HTMLElement)) {
-            console.error("Error: the argument must be an HTML element");
-            return;
+            console.error('Error: the argument must be an HTML element')
+            return
         }
 
-        this.screen = screen;
-        this.init();
+        this.screen = screen
+        this.init()
     }
 
     init() {
-        window.application.screen = difficulty;
-        this.renderScreen();
+        window.application.screen = difficulty
+        this.renderScreen()
         // this.screen.appendChild(templateEngine(window.application.screen));
 
-        const radioButtons = this.screen.querySelectorAll('[id^="radio_"]');
-        const startButton = this.screen.querySelector('#btn');
+        const radioButtons = this.screen.querySelectorAll('[id^="radio_"]')
+        const startButton = this.screen.querySelector('#btn')
 
         radioButtons.forEach((radioButton) => {
             radioButton.addEventListener('change', (event) => {
                 if (event.target.checked) {
-                    window.application.difficultyLevel = radioButton.nextSibling.textContent;
+                    window.application.difficultyLevel =
+                        radioButton.nextSibling.textContent
                 }
             })
-        });
+        })
 
         startButton.addEventListener('click', () => {
-            console.log(startButton);
-            window.application.screen = temporary;
-            this.renderScreen();
+            const numberOfCards = 18
+            let suits = ['club', 'spade', 'heart', 'diamond']
+            let ranks = [
+                'king',
+                'queen',
+                'jack',
+                '10',
+                '9',
+                '8',
+                '7',
+                '6',
+                '5',
+                '4',
+                '3',
+                '2',
+                '1',
+            ]
+            let cardDeck = []
+
+            while (cardDeck.length < numberOfCards) {
+                let suit = suits[Math.floor(Math.random() * suits.length)]
+                let rank = ranks[Math.floor(Math.random() * ranks.length)]
+                let card = '#' + suit + '_' + rank
+
+                if (!cardDeck.includes(card)) {
+                    cardDeck.push(card)
+                }
+            }
+
+            window.application.screen = playField
+            this.renderScreen()
+            const field = document.querySelector('.field__cards')
+            for (let index = 0; index < numberOfCards; index++) {
+                field.appendChild(templateEngine(oneCard))
+                let cardId = document.querySelector('#card')
+                let cardFront = cardId.querySelector('.imgBack')
+                cardFront.setAttribute(
+                    'href',
+                    './src/img/svg-cards.svg' + cardDeck[index]
+                )
+                cardId.setAttribute('id', `card${index}`)
+            }
         })
     }
 
     renderScreen() {
         while (this.screen.firstChild) {
-            this.screen.removeChild(this.screen.firstChild);
+            this.screen.removeChild(this.screen.firstChild)
         }
-        this.screen.appendChild(templateEngine(window.application.screen));
+        this.screen.appendChild(templateEngine(window.application.screen))
     }
-
-
 }
--- src/template-engine.js
+++ src/template-engine.js
@@ -1,41 +1,51 @@
 function templateEngine(block) {
-    if ((block === undefined) || (block === null) || (block === false)) {
-        return document.createTextNode('');
+    if (block === undefined || block === null || block === false) {
+        return document.createTextNode('')
     }
-    if ((typeof block === 'string') || (typeof block === 'number') || (block === true)) {
-        return document.createTextNode(block);
+    if (
+        typeof block === 'string' ||
+        typeof block === 'number' ||
+        block === true
+    ) {
+        return document.createTextNode(block)
     }
     if (Array.isArray(block)) {
-        const fragment = document.createDocumentFragment();
+        const fragment = document.createDocumentFragment()
 
-        block.forEach(item => {
-            const el = templateEngine(item);
+        block.forEach((item) => {
+            const el = templateEngine(item)
 
-            fragment.appendChild(el);
-        });
+            fragment.appendChild(el)
+        })
 
-        return fragment;
+        return fragment
     }
 
-    const element = document.createElement(block.tag);
+    let element
+    if (block.tag === 'svg' || block.tag === 'use') {
+        element = document.createElementNS(
+            'http://www.w3.org/2000/svg',
+            block.tag
+        )
+    } else {
+        element = document.createElement(block.tag)
+    }
 
     if (block.cls) {
-        element.classList.add(
-            ...[].concat(block.cls).filter(Boolean)
-        );
+        element.classList.add(...[].concat(block.cls).filter(Boolean))
     }
 
     if (block.attrs) {
-        const keys = Object.keys(block.attrs);
+        const keys = Object.keys(block.attrs)
 
-        keys.forEach(key => {
-            element.setAttribute(key, block.attrs[key]);
-        });
+        keys.forEach((key) => {
+            element.setAttribute(key, block.attrs[key])
+        })
     }
 
-    const content = templateEngine(block.content);
+    const content = templateEngine(block.content)
 
-    element.appendChild(content);
+    element.appendChild(content)
 
-    return element;
+    return element
 }
